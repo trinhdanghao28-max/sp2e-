dbf1fd9913b975cf0c23f4976d52117a8306f392
trinhdanghao28-max <trinhdanghao28@gmail.com>
2026-07-18 11:20:22 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,57 @@
+const SCORE_TABLES = {
+
+    2026: {
+
+        A00: [
+            { sp2eMin:25.24, sp2eMax:30.00, thptMin:27.75, thptMax:30.00 },
+            { sp2eMin:18.29, sp2eMax:25.24, thptMin:22.85, thptMax:27.75 },
+            { sp2eMin:14.60, sp2eMax:18.29, thptMin:18.95, thptMax:22.85 },
+            { sp2eMin:10.96, sp2eMax:14.60, thptMin:15.00, thptMax:18.95 }
+        ],
+
+        A01: [
+            { sp2eMin:25.43, sp2eMax:30.00, thptMin:26.26, thptMax:30.00 },
+            { sp2eMin:17.13, sp2eMax:25.43, thptMin:18.34, thptMax:26.26 },
+            { sp2eMin:13.61, sp2eMax:17.13, thptMin:15.00, thptMax:18.34 }
+        ],
+
+        B00: [
+            { sp2eMin:22.77, sp2eMax:30.00, thptMin:27.74, thptMax:30.00 },
+            { sp2eMin:13.41, sp2eMax:22.77, thptMin:19.27, thptMax:27.74 },
+            { sp2eMin:10.02, sp2eMax:13.41, thptMin:15.00, thptMax:19.27 }
+        ],
+
+        B03: [
+            { sp2eMin:19.08, sp2eMax:30.00, thptMin:25.31, thptMax:30.00 },
+            { sp2eMin:14.23, sp2eMax:19.08, thptMin:19.75, thptMax:25.31 },
+            { sp2eMin:10.48, sp2eMax:14.23, thptMin:15.00, thptMax:19.75 }
+        ],
+
+        C00: [
+            { sp2eMin:25.94, sp2eMax:30.00, thptMin:28.00, thptMax:30.00 },
+            { sp2eMin:22.95, sp2eMax:25.94, thptMin:24.75, thptMax:28.00 },
+            { sp2eMin:19.37, sp2eMax:22.95, thptMin:20.00, thptMax:24.75 },
+            { sp2eMin:12.86, sp2eMax:19.37, thptMin:15.00, thptMax:20.00 }
+        ],
+
+        D01: [
+            { sp2eMin:23.76, sp2eMax:30.00, thptMin:26.00, thptMax:30.00 },
+            { sp2eMin:17.76, sp2eMax:23.76, thptMin:20.00, thptMax:26.00 },
+            { sp2eMin:11.92, sp2eMax:17.76, thptMin:15.00, thptMax:20.00 }
+        ],
+
+        D14: [
+            { sp2eMin:25.96, sp2eMax:30.00, thptMin:27.00, thptMax:30.00 },
+            { sp2eMin:19.79, sp2eMax:25.96, thptMin:20.25, thptMax:27.00 },
+            { sp2eMin:12.87, sp2eMax:19.79, thptMin:15.00, thptMax:20.25 }
+        ],
+
+        X70: [
+            { sp2eMin:24.83, sp2eMax:30.00, thptMin:26.85, thptMax:30.00 },
+            { sp2eMin:19.07, sp2eMax:24.83, thptMin:19.90, thptMax:26.85 },
+            { sp2eMin:14.73, sp2eMax:19.07, thptMin:15.00, thptMax:19.90 }
+        ]
+
+    }
+
+};
